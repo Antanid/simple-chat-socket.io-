daa6a27fa3abaaa7e206dbf8a64bf3ee5cd4542f
Antanid <dbalakleenko1@gmail.com>
2023-03-10 19:12:52 +0200

    end

--- src/component/JoinBlock.jsx
+++ src/component/JoinBlock.jsx
@@ -1,24 +1,43 @@
-import React from 'react';
-import socket from '../socket';
+import axios from "axios";
+import React, { useState } from "react";
 
-const JoinBlock = () => {
+const JoinBlock = ({ onLogin }) => {
+  const [roomId, setRoomId] = useState("");
+  const [userName, setUserName] = useState("");
+  const [isLoading, setLoading] = useState(true);
+
+  const onChangeRoomId = (e) => {
+    setRoomId(e.target.value);
+  };
+  const onChangeName = (e) => {
+    setUserName(e.target.value);
+  };
+  const onEnter = async () => {
+    if (!roomId || !userName) {
+      alert("Вы не заполнили все поля");
+    }
+    try {
+      setLoading(false);
+      const obj = {
+        roomId,
+        userName,
+      }
+      await axios
+        .post("/rooms", obj)
+        .then(onLogin(obj));
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div className="join-block">
-    <input
-    placeholder="Room ID"
-    type='text'
-    value=''
-    />
-     <input
-    placeholder="Your name"
-    type='text'
-    value=''
-    />
-    <button className="btn btn-success">
-      Sign in
-    </button>
-  </div>
-  )
-}
+      <input onChange={onChangeRoomId} placeholder="Room ID" type="text" value={roomId} />
+      <input onChange={onChangeName} placeholder="Your name" type="text" value={userName} />
+      <button disabled={!isLoading} onClick={onEnter} className="btn btn-success">
+        {isLoading ? "Sign in" : "Waiting..."}
+      </button>
+    </div>
+  );
+};
 
-export default JoinBlock
+export default JoinBlock;
